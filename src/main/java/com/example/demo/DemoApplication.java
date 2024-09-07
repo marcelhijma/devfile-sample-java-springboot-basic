00405b0e5e183615acf9b94fbd0ab640f8b4d691
juancvilla <juancvilla@gmail.com>
2024-09-07 11:32:56 -0400

    1change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! JUAN";
     }
 
     public static void main(String[] args) {
